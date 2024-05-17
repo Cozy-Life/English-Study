28388c1be9448b04f0bed27778106497a521a720
Cozy-Life <takeshiokada@okatakese.com>
2024-05-17 15:35:54 +0900

    vuetifyをプロジェクトに導入

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="login-wrapper">
+    <div>
+        <p>ログイン</p>
+    </div>
+    <div>
+        <p>ユーザID</p>
+        <input type="text" />
+    </div>
+    <div>
+        <p>パスワード</p>
+        <input type="password" />
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+</style>
