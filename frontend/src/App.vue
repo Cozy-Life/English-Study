cc25d437360405796e77b5636951fdac06aab831
Cozy-Life <takeshiokada@okatakese.com>
2024-06-02 16:35:22 +0900

    ログイン画面と遷移を実現 API通信は除く

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,51 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { computed, ref } from 'vue';
+import { RouterLink, RouterView, useRoute } from 'vue-router'
+
+const route = useRoute();
+const drawer = ref(false);
+const showNavi = computed(() => {
+  const matched = route.matched;
+  const currendComponentName = matched.length > 0 ? matched[matched.length - 1].name : 'Login' 
+  return currendComponentName !== 'Login'
+});
+
 </script>
 
 <template>
-  <header>
-  
-  </header>
-  <main>
-    <v-btn block rounded="0" size="x-large">Test Button</v-btn>
-    <v-icon icon="mdi-home"></v-icon>
-  </main>
-  <footer>
-
-  </footer>
+  <v-app>
+    <v-app-bar color="primary">
+      <v-app-bar-nav-icon
+        variant="text"
+        @click.stop="drawer = !drawer">
+      </v-app-bar-nav-icon>
+      <v-app-bar-title>
+        英単語アプリケーション
+      </v-app-bar-title>
+    </v-app-bar>
+    <div v-if="showNavi">
+      <v-navigation-drawer v-model="drawer">
+        <v-list>
+          <v-list-item
+            title="Home"
+            value="home"
+            @click="$router.push('/home')">
+          </v-list-item>
+          <v-list-item
+            title="Logout"
+            value="logout"
+            @click="$router.push('/')">
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </div>
+    <v-main>
+      <router-view />
+    </v-main>
+    <v-footer app color="primary">
+      岡田システム設計事務所 (c) 2024
+    </v-footer>
+  </v-app>
 
 </template>
 
--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -14,5 +14,20 @@ export default createVuetify({
         sets: {
             mdi,
         }
+    },
+    theme: {
+        themes: {
+            light: { // テーマ名
+                colors: { // 以下で色を指定する（下記はデフォルト色）{
+                    primary: '#1976D2',
+                    secondary: '#424242',
+                    accent: '#82B1FF',
+                    error: '#FF5252',
+                    info: '#2196F3',
+                    success: '#4CAF50',
+                    warning: '#FFC107',
+                },
+            },
+        },
     }
 })
